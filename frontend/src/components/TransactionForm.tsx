1f8fec1c1e1a1c1dee463bc67e8e57ca837bce2e
otaviootavio <otaviovacari@usp.br>
2023-07-13 00:29:02 -0300

    add send token interface

--- frontend/src/components/TransactionForm.tsx
+++ frontend/src/components/TransactionForm.tsx
@@ -14,6 +14,7 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 
 const TransactionForm = () => {
   const account = useAccount()
+  const [recipient, setRecipient] = useState<string>('')
   const [amount, setAmount] = useState<bigint>(0n);
   const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
   const [snackbarText, setSnackbarText] = useState<string>('');
@@ -54,20 +55,6 @@ const TransactionForm = () => {
   })
   const waitApprove = useWaitForTransaction({ hash: approveWrite.data?.hash })
 
-  const sharesData = useContractRead({
-    address: contractConfig.address as Address,
-    abi: contractConfig.abi,
-    functionName: 'balanceOf',
-    args: [account.address],
-    watch: true
-  })
-
-  // const symbol_token = useContractRead({
-  //   address: contractConfig.address as Address,
-  //   abi: contractConfig.abi,
-  //   functionName: 'symbol_',
-  //   watch: true
-  // })
 
   const depositWrite = useContractWrite({
     address: contractConfig.address as Address,
@@ -121,6 +108,32 @@ const TransactionForm = () => {
     })
   };
 
+  const sendTokenWrite = useContractWrite({
+    address: contractConfig.address as Address,
+    abi: contractConfig.abi,
+    functionName: 'transfer',
+    onError(error) {
+      setSeveritySnackbar('error')
+      setOpenSnackbar(true)
+      setSnackbarText(error.message || '')
+    },
+    onSuccess(data) {
+      setSeveritySnackbar('success')
+      setOpenSnackbar(true)
+      setSnackbarText('Transferencia efetuada com sucesso!')
+    },
+  })
+  const waitSendToken = useWaitForTransaction({ hash: sendTokenWrite.data?.hash })
+
+  const handleSendToken = () => {
+    sendTokenWrite.write({
+      args: [recipient as Address, BigInt(amount)],
+    })
+  };
+
+  const handleRecipientChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setRecipient(event.target.value);
+  };
   return (
     <Box>
       <Stack direction="row" alignItems="center" spacing={2}>
@@ -130,35 +143,73 @@ const TransactionForm = () => {
           type="number"
           InputLabelProps={{
             shrink: true,
-          }}  
+          }}
           variant="standard"
-          onChange={handleAmountChange} />
+          onChange={handleAmountChange}
+        />
+        <TextField
+          label="Recipient"
+          type="text"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="standard"
+          onChange={handleRecipientChange}
+        />
 
-        <Typography variant="body1">Saldo: {balance.data?.formatted ?? 0}</Typography>
-        <Typography variant="body1">Shares: {sharesData.data?.toString() ?? 0}</Typography>
-        {/* <Typography variant="body1">Shares: {symbol_token.data?.toString() ?? 0}</Typography> */}
-
-        <div>{}</div>
+        <Typography variant="body1">
+          Saldo: {balance.data?.formatted ?? 0}
+        </Typography>
       </Stack>
       <br />
       <Stack direction="row" spacing={2}>
-          <LoadingButton loading={waitDeposit.isLoading || depositWrite.isLoading} variant="outlined" color="primary" onClick={handleDeposit}>
-            Deposit
-          </LoadingButton>
-          <LoadingButton loading={waitWithdraw.isLoading || withdrawWrite.isLoading} variant="outlined" color="secondary" onClick={handleWithdraw}>
-            Withdraw
-          </LoadingButton>
-          <LoadingButton loading={waitApprove.isLoading || approveWrite.isLoading} variant="outlined" color="warning" onClick={handleApprove}>
-            Approve
-          </LoadingButton>
+        <LoadingButton
+          loading={waitDeposit.isLoading || depositWrite.isLoading}
+          variant="outlined"
+          color="primary"
+          onClick={handleDeposit}
+        >
+          Deposit
+        </LoadingButton>
+        <LoadingButton
+          loading={waitWithdraw.isLoading || withdrawWrite.isLoading}
+          variant="outlined"
+          color="secondary"
+          onClick={handleWithdraw}
+        >
+          Withdraw
+        </LoadingButton>
+        <LoadingButton
+          loading={waitApprove.isLoading || approveWrite.isLoading}
+          variant="outlined"
+          color="warning"
+          onClick={handleApprove}
+        >
+          Approve
+        </LoadingButton>
+        <LoadingButton
+          loading={waitSendToken.isLoading || waitSendToken.isLoading}
+          variant="outlined"
+          color="warning"
+          onClick={handleSendToken}
+        >
+          SendToken
+        </LoadingButton>
       </Stack>
-      <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-        <Alert onClose={handleCloseSnackbar} severity={severitySnackbar} sx={{ width: '100%' }}>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={severitySnackbar}
+          sx={{ width: "100%" }}
+        >
           {snackbarText}
         </Alert>
       </Snackbar>
     </Box>
-    
   );
 };
 
